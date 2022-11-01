47f7c978243d428cc517cfb842c025da4de55646
KimHuiMang <piyrw9754@gmail.com>
2022-11-01 14:32:58 +0900

    Create Homebase Reservation Entity

---
+++ src/main/java/msg/team1/Hi/domain/reserve/entity/Reservation.java
@@ -0,0 +1,29 @@
+package msg.team1.Hi.domain.reserve.entity;
+
+import lombok.*;
+import msg.team1.Hi.domain.homebase.entity.Homebase;
+import msg.team1.Hi.domain.user.entity.User;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "reservation")
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "reservation_idx", nullable = false)
+    private Integer reservation_idx;
+
+    @OneToOne
+    @JoinColumn(name = "user_idx")
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "homebase_idx")
+    private Homebase homebase;
+}
--- src/main/java/msg/team1/Hi/domain/user/controller/UserController.java
+++ src/main/java/msg/team1/Hi/domain/user/controller/UserController.java
@@ -8,4 +8,5 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ResponseBody
 @RequestMapping("/user")
 public class UserController {
+
 }
