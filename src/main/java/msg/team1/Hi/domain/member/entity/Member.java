fae555a07f6e1566c8097fd16e8d611d73def9a7
KimHuiMang <piyrw9754@gmail.com>
2022-11-11 12:27:09 +0900

    Member 생성자 삭제 Builder 적용

--- src/main/java/msg/team1/Hi/domain/member/entity/Member.java
+++ src/main/java/msg/team1/Hi/domain/member/entity/Member.java
@@ -4,12 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import msg.team1.Hi.domain.member.dto.request.SignUpRequest;
 import msg.team1.Hi.global.role.Role;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Getter @Builder
@@ -40,21 +38,7 @@ public class Member {
     @Enumerated(EnumType.STRING)
     private  Role role;
 
-    public Member(SignUpRequest request) {
-        this.email = request.getEmail();
-        this.password = request.getPassword();
-        this.name = request.getName();
-        this.number = request.getNumber();
-        this.role = Role.from(request.getRole());
-    }
 
-    public Member(String email, String password, String number, String name, @NotEmpty(message = "역할을 지정해주십시요.") Role role) {
-        this.email = email;
-        this.password = password;
-        this.number = number;
-        this.name = name;
-        this.role = role;
-    }
 
     public void encryptPassword(PasswordEncoder passwordEncoder) {
         password = passwordEncoder.encode(password);
