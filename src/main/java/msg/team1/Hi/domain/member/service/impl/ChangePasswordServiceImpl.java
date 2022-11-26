8d426f4aace8bdc67042d5fcf3270d1048e550e5
KimHuiMang <piyrw9754@gmail.com>
2022-11-26 19:45:54 +0900

    validAuth void로 변경 execute 로직 수정

--- src/main/java/msg/team1/Hi/domain/member/service/impl/ChangePasswordServiceImpl.java
+++ src/main/java/msg/team1/Hi/domain/member/service/impl/ChangePasswordServiceImpl.java
@@ -22,17 +22,15 @@ public class ChangePasswordServiceImpl implements ChangePasswordService {
     @Override
     public void execute(ChangePasswordRequest changePasswordRequest) {
         Member member = memberUtil.currentMember();
-        if(validateAuth(member.getEmail())) {
-            member.updatePassword(passwordEncoder.encode(changePasswordRequest.getPassword()));
-        }
+        validateAuth(member.getEmail());
+        member.updatePassword(passwordEncoder.encode(changePasswordRequest.getPassword()));
     }
 
-    public Boolean validateAuth(String email) {
+    public void validateAuth(String email) {
         EmailAuth emailAuth = emailAuthRepository.findById(email)
                 .orElseThrow(() -> new NotVerifyEmailException("검증되지 않은 이메일입니다."));
         if(!emailAuth.getAuthentication()){
             throw new NotVerifyEmailException("검증되지 않은 이메일입니다.");
         }
-        return true;
     }
 }
