07a30147a76fdf5e70a576b9a567900f54a428c8
KimHuiMang <piyrw9754@gmail.com>
2022-12-17 23:13:00 +0900

    :sparkles: controller notice 추가
    student, teacher별로 공지사항 컨트롤러 추가

---
+++ src/main/java/msg/team1/Hi/domain/notice/controller/student/StudentNoticeController.java
@@ -0,0 +1,41 @@
+package msg.team1.Hi.domain.notice.controller.student;
+
+import lombok.RequiredArgsConstructor;
+import msg.team1.Hi.domain.notice.dto.request.NoticeRequest;
+import msg.team1.Hi.domain.notice.dto.response.GetIdNoticeResponse;
+import msg.team1.Hi.domain.notice.dto.response.GetNoticeResponse;
+import msg.team1.Hi.domain.notice.service.NoticeService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/student/notice")
+@RequiredArgsConstructor
+public class StudentNoticeController {
+
+    private final NoticeService noticeService;
+
+    @PostMapping
+    public ResponseEntity<Void> createNoticeAdmin(@Valid @RequestBody NoticeRequest noticeRequest) {
+        noticeService.createNotice(noticeRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<GetNoticeResponse>> getAllNoticeAdmin(@PageableDefault(size = 6) Pageable pageable) {
+        Page<GetNoticeResponse> result = noticeService.getAllNotice(pageable);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GetIdNoticeResponse> getNoticeById(@PathVariable Long id) {
+        GetIdNoticeResponse notice = noticeService.getNoticeById(id);
+        return ResponseEntity.ok().body(notice);
+    }
+}
