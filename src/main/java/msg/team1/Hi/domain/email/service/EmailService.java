a894dd9da12c5697eee740836f583316c7e1e151
aesperer <piyrw9754@gmail.com>
2023-02-01 00:06:49 +0900

    :fire: 인터페이스에 public? .. 삭제
    접근제어자 삭제

--- src/main/java/msg/team1/Hi/domain/email/service/EmailService.java
+++ src/main/java/msg/team1/Hi/domain/email/service/EmailService.java
@@ -3,6 +3,6 @@ package msg.team1.Hi.domain.email.service;
 import msg.team1.Hi.domain.email.dto.request.EmailSentDto;
 
 public interface EmailService {
-    public void sendEmail(EmailSentDto emailSentDto);
-    public void checkEmail(String email , String authKey);
+    void sendEmail(EmailSentDto emailSentDto);
+    void checkEmail(String email , String authKey);
 }
