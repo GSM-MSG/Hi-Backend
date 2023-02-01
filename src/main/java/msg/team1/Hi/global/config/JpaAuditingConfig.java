654da445842286fae3348e993ae876bf8da96b3e
aesperer <piyrw9754@gmail.com>
2023-02-01 11:12:49 +0900

    :recycle: Redis 설정 리팩터링
    리팩터링

--- src/main/java/msg/team1/Hi/global/config/JpaAuditingConfig.java
+++ src/main/java/msg/team1/Hi/global/config/JpaAuditingConfig.java
@@ -1,4 +1,4 @@
-package msg.team1.Hi.global.config.jpa;
+package msg.team1.Hi.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
