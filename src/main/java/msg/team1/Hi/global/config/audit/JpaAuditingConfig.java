b220b9d042bd64be0553087b9bea290ce4af797e
aesperer <piyrw9754@gmail.com>
2023-02-07 15:04:20 +0900

    :sparkles: CORS 설정
    CORS 설정

--- src/main/java/msg/team1/Hi/global/config/audit/JpaAuditingConfig.java
+++ src/main/java/msg/team1/Hi/global/config/audit/JpaAuditingConfig.java
@@ -1,4 +1,4 @@
-package msg.team1.Hi.global.config;
+package msg.team1.Hi.global.config.audit;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
