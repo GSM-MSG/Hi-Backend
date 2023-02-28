b2cd30b209da7f91470d83314cb2213a85d9c306
aesperer <piyrw9754@gmail.com>
2023-02-28 18:11:14 +0900

    :recycle: 공지사항 리팩터링
    리팩터링

--- src/main/java/msg/team1/Hi/domain/notice/presentation/controller/student/StudentNoticeController.java
+++ src/main/java/msg/team1/Hi/domain/notice/presentation/controller/student/StudentNoticeController.java
@@ -3,7 +3,7 @@ package msg.team1.Hi.domain.notice.presentation.controller.student;
 import lombok.RequiredArgsConstructor;
 import msg.team1.Hi.domain.notice.presentation.dto.response.GetIdNoticeResponse;
 import msg.team1.Hi.domain.notice.presentation.dto.response.GetNoticeResponse;
-import msg.team1.Hi.domain.notice.service.NoticeService;
+import msg.team1.Hi.domain.notice.service.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,17 +17,19 @@ import java.util.List;
 @RequestMapping("/student/notice")
 public class StudentNoticeController {
 
-    private final NoticeService noticeService;
+    private final GetNoticeService getNoticeService;
+    private final GetAllNoticeService getAllNoticeService;
 
     @GetMapping
     public ResponseEntity<List<GetNoticeResponse>> getAllNotice() {
-        List<GetNoticeResponse> result = noticeService.getAllNotice();
-        return ResponseEntity.ok().body(result);
+        List<GetNoticeResponse> responses = getAllNoticeService.execute();
+        return ResponseEntity.ok(responses);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<GetIdNoticeResponse> getNoticeById(@PathVariable Integer id) {
-        GetIdNoticeResponse notice = noticeService.getNoticeById(id);
-        return ResponseEntity.ok().body(notice);
+    public ResponseEntity<GetIdNoticeResponse> getNoticeById(@PathVariable Long id) {
+        GetIdNoticeResponse response = getNoticeService.execute(id);
+        return ResponseEntity.ok(response);
     }
+
 }
