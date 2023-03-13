4e91be3734e100fdc91d97dd6fd3a475ffb9afbc
aesperer <piyrw9754@gmail.com>
2023-03-13 19:36:54 +0900

    :sparkles: 멤버 검색 api

--- src/main/java/msg/team1/Hi/domain/member/presentation/MemberController.java
+++ src/main/java/msg/team1/Hi/domain/member/presentation/MemberController.java
@@ -2,11 +2,16 @@ package msg.team1.Hi.domain.member.presentation;
 
 import lombok.RequiredArgsConstructor;
 import msg.team1.Hi.domain.member.presentation.dto.request.ChangePasswordRequest;
+import msg.team1.Hi.domain.member.presentation.dto.request.SearchMemberRequest;
+import msg.team1.Hi.domain.member.presentation.dto.response.MemberInfoResponse;
 import msg.team1.Hi.domain.member.service.ChangePasswordService;
+import msg.team1.Hi.domain.member.service.SearchMemberService;
 import msg.team1.Hi.domain.member.service.WithdrawService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,11 +20,18 @@ public class MemberController {
 
     private final ChangePasswordService changePasswordService;
     private final WithdrawService withdrawService;
+    private final SearchMemberService searchMemberService;
 
     @PatchMapping("/change-pw")
-    public ResponseEntity<Void> execute(@RequestBody @Validated ChangePasswordRequest changePasswordRequest) {
+    public ResponseEntity<Void> changePassword(@RequestBody @Validated ChangePasswordRequest changePasswordRequest) {
         changePasswordService.execute(changePasswordRequest);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/search")
+    public ResponseEntity<List<MemberInfoResponse>> searchMember(@RequestBody @Validated SearchMemberRequest searchMemberRequest) {
+        List<MemberInfoResponse> responses = searchMemberService.execute(searchMemberRequest);
+        return ResponseEntity.ok(responses);
     }
 
     @DeleteMapping
