103ebeb403e607cf1ed5a733a2c5bf79c16321c7
aesperer <piyrw9754@gmail.com>
2023-03-14 19:55:17 +0900

    :sparkles: 홈베이스 예약 나가기 서비스

--- src/main/java/msg/team1/Hi/domain/reservation/service/ExitReservationService.java
+++ src/main/java/msg/team1/Hi/domain/reservation/service/ExitReservationService.java
@@ -5,6 +5,8 @@ import msg.team1.Hi.domain.homebase.exception.ForbiddenHomeBaseReservationExcept
 import msg.team1.Hi.domain.member.entity.Member;
 import msg.team1.Hi.domain.member.entity.enum_type.UseStatus;
 import msg.team1.Hi.domain.member.repository.MemberRepository;
+import msg.team1.Hi.domain.reservation.entity.Reservation;
+import msg.team1.Hi.domain.reservation.exception.NotFoundReservationException;
 import msg.team1.Hi.domain.reservation.repository.ReservationRepository;
 import msg.team1.Hi.global.annotation.TransactionalService;
 import msg.team1.Hi.global.util.MemberUtil;
@@ -17,14 +19,19 @@ public class ExitReservationService {
     private final MemberRepository memberRepository;
     private final MemberUtil memberUtil;
 
+    private void verifyMember(Reservation reservation, Member member){
+        if(!memberRepository.existsByReservation(reservation))
+            throw new ForbiddenHomeBaseReservationException("홈베이스를 예약하고 있는 상황이 아닙니다.");
 
-    public void execute(){
-        Member currentMember = memberUtil.currentMember();
-
-
-
-        if(currentMember.getReservation() == null)
+        if(reservationRepository.existsByIdAndRepresentative(reservation.getId(), member))
             throw new ForbiddenHomeBaseReservationException("나갈 수 있는 자격이 없습니다.");
+    }
+
+    public void execute(Long reservationId){
+        Reservation reservation = reservationRepository.findById(reservationId)
+                .orElseThrow(() -> new NotFoundReservationException("존재하지 않는 예약현황"));
+        Member currentMember = memberUtil.currentMember();
+        verifyMember(reservation, currentMember);
         memberRepository.save(currentMember.updateReservationAndUseStatus(UseStatus.AVAILABLE, null));
     }
 }
